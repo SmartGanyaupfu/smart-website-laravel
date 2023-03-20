78fb761e3ee2589d93f08447ec000178e3fc681e
SmartGanyaupfu <smartganyaupfu@gmail.com>
2023-03-20 21:11:11 +0200

    Added the logout feature on new dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DemoController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//Route::get('/about',  'Index')->name('about.page');
+
+Route::controller(DemoController::class)->group(function () {
+    Route::get('/about',  'Index')->name('about.page');
+    Route::get('/contact',  'ContactUs')->name('contact.page');
+});
 
 Route::get('/dashboard', function () {
     return view('admin.index');
@@ -26,6 +34,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+   // Route::delete('/admin/logout', [ProfileController::class, 'destroy'])->name('admin.logout');
+});
+
+
+
+Route::controller(AdminController::class)->group(function () {
+    Route::get('/admin/logout',  'destroy')->name('admin.logout');
+    //Route::get('/contact',  'ContactUs')->name('contact.page');
 });
 
 require __DIR__.'/auth.php';
