8877ae705a5cf3a5956094301a2faf64e59371ec
SmartGanyaupfu <smartganyaupfu@gmail.com>
2023-03-20 14:12:24 +0200

    First Commit

---
+++ app/Http/Controllers/TestAPIController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TestModel;
+use Illuminate\Http\Request;
+
+class TestAPIController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TestModel $testModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TestModel $testModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TestModel $testModel)
+    {
+        //
+    }
+}
---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+<img src="{{asset('logo/SG_logo_colour.png')}}" width="100px"/>
